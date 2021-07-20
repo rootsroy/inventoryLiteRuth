b7e5bfb9b919fc541d99be007c8f18527b89716a
Richie Tauch <rtauc001@gmail.com>
2021-07-19 19:57:02 -0700

    scaffolding out initial backend

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+-- DROP DATABASE
+DROP DATABASE IF EXISTS inventorytracker_db;
+
+-- CREATE DATABASE
+CREATE DATABASE inventorytracker_db;
+
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes');
+//const routes = require('./routes');
 // import sequelize connection
 
 const app = express();
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+//app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
