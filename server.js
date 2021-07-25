fc7e77febc439879ffd2d115f8d0626feffbf8d1
Ruth Torres <ruths.roy@gmail.com>
2021-07-25 16:35:27 -0500

    rename routes folder to controllers,  created public folder and style.css file, views/layout/main.hadlebars and views/homepage.hadlebars

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const routes = require('./routes');
+const path = require("path");
+const express = require("express");
+const routes = require("./controllers");
 //import sequelize connection
 
 const app = express();
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
