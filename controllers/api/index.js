9bc260f092d40490a82671c2ca961ee0096b1e24
Ruth Torres <ruths.roy@gmail.com>
2021-07-25 19:26:31 -0500

    adding home, login and product  html

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,16 +1,13 @@
-const router = require('express').Router();
-const userRoutes = require('./user-routes');
-const typeRoutes = require('./type-routes');
-const itemRoutes = require('./item-routes');
-const tagRoutes = require('./tag-routes');
-;
-
+const router = require("express").Router();
+const userRoutes = require("./user-routes");
+const typeRoutes = require("./type-routes");
+const itemRoutes = require("./item-routes");
+const tagRoutes = require("./tag-routes");
 
 //Url...
-router.use('/users', userRoutes);
-router.use('/types', typeRoutes);
-router.use('/items', itemRoutes);
-router.use('/tags', tagRoutes);
-
+router.use("/users", userRoutes);
+router.use("/types", typeRoutes);
+router.use("/items", itemRoutes);
+router.use("/tags", tagRoutes);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,18 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const path = require("path");
+
+router.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/login.html"));
+});
+
+router.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+router.get("/product", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/product.html"));
+});
 
 router.use("/api", apiRoutes);
 
