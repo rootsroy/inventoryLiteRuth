0e9bbc15a9074222c48f064626419a49c2089ed6
Diana Mauricio <dwmauricio@g.ucla.edu>
2021-07-26 19:35:22 -0700

    User pw

--- models/user.js
+++ models/user.js
@@ -39,6 +39,12 @@ User.init(
   {
       hooks: {
 
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password,10);
+            return newUserData;
+        },
+}
+      }
       },
       sequelize,
       timestamps: false,
