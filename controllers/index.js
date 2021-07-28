67ae26fb893484b3ada6b41db18702a5cdc4253b
Ruth Torres <ruths.roy@gmail.com>
2021-07-28 16:09:46 -0500

    finalized ui

--- controllers/index.js
+++ controllers/index.js
@@ -6,6 +6,10 @@ router.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/login.html"));
 });
 
+router.get("/signup", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/signup.html"));
+});
+
 router.get("/home", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/home.html"));
 });
