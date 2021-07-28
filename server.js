7b8388c6b9c798516f919ee889a567b6cf729130
Richie Tauch <rtauc001@gmail.com>
2021-07-28 14:39:47 -0700

    login

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(routes);
 
 //turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening inventoryLite"));
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
 
 //app.listen(PORT, () =>{
