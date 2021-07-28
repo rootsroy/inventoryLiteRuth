b561b6f5e4d3b66743c04cd057f0a9958789d1e7
Ruth Torres <ruths.roy@gmail.com>
2021-07-28 18:11:40 -0500

    chore files

--- controllers/index.js
+++ controllers/index.js
@@ -1,23 +1,8 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
-const path = require("path");
+const htmlRoutes = require("./html");
 
-router.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/login.html"));
-});
-
-router.get("/signup", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/signup.html"));
-});
-
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/home.html"));
-});
-
-router.get("/product", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/product.html"));
-});
-
+router.use("/", htmlRoutes);
 router.use("/api", apiRoutes);
 
 router.use((req, res) => {
